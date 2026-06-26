1941dec2ca16fd5a5bcfbd40c19dd1fe171e01cd
Gabriel-Lima231 <lima.gabriel3@aluno.ifsp.edu.br>
2026-06-25 23:14:07 -0300

    Add files via upload

---
+++ Lista2_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro valor: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("Maior: " + n1);
+            }
+            else if (n2 > n1)
+            {
+                Console.WriteLine("Maior: " + n2);
+            }
+            else
+            {
+                Console.WriteLine("Os números são idênticos.");
+            }
+        }
+    }
+}
