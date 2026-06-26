e5a9dd2f2d2be13d668896f4ca93ea61baa34de4
Gabriel-Lima231 <lima.gabriel3@aluno.ifsp.edu.br>
2026-06-25 23:09:12 -0300

    Add files via upload

---
+++ Lista2-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro valor: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("Maior: " + n1);
+            }
+            else
+            {
+                Console.WriteLine("Maior: " + n2);
+            }
+        }
+    }
+}
